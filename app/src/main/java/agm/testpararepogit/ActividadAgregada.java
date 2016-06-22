5bf908670cc6d6e10bbc4b82e9c68a883a142b93
Seb.angulo <thegear17@gmail.com>
2016-06-22 15:57:33 -0400

    se agrego una actividad llamada ActividadAgregada

---
+++ app/src/main/java/agm/testpararepogit/ActividadAgregada.java
@@ -0,0 +1,13 @@
+package agm.testpararepogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadAgregada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_agregada);
+    }
+}
